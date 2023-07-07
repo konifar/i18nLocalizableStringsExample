fbd5956de983616469981c33e82e3941ab03efb5
konifar <yahpeycoy0403@gmail.com>
2023-07-08 04:15:55 +0900

    Initial Commit

---
+++ i18nLocalizableStringsExample/i18nLocalizableStringsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  i18nLocalizableStringsExampleApp.swift
+//  i18nLocalizableStringsExample
+//
+//  Created by Yusuke Konishi on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct i18nLocalizableStringsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
